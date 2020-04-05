cdcac906a4bd08a251e2a0d161799ef64e326d91
leseastingray <lesea@my.lanecc.edu>
2020-04-04 17:04:56 -0700

    added Admin controller and views

---
+++ FishClubWebsite/Controllers/AdminController.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using FishClubWebsite.Models;
+using FishClubWebsite.Repositories;
+using FishClubWebsite.Data;
+using FishClubWebsite.Infrastructure;
+
+namespace FishClubWebsite.Controllers
+{
+    [Authorize(Roles = "Admins")]
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+        private IUserValidator<AppUser> userValidator;
+        private IPasswordValidator<AppUser> passwordValidator;
+        private IPasswordHasher<AppUser> passwordHasher;
+
+        // Constructor, incorporates roles
+        public AdminController(UserManager<AppUser> uMgr,
+            RoleManager<IdentityRole> rMgr,
+            IUserValidator<AppUser> userValid,
+            IPasswordValidator<AppUser> passValid,
+            IPasswordHasher<AppUser> passHash)
+        {
+            userManager = uMgr;
+            roleManager = rMgr;
+            userValidator = userValid;
+            passwordValidator = passValid;
+            passwordHasher = passHash;
+        }
+        [AllowAnonymous]
+        // Main Account page - Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*
+         * Account Administration
+         */
+         [AllowAnonymous]
+        // Show Accounts page - lists member accounts
+        public ViewResult ShowAccounts()
+        {
+            return View(userManager.Users);
+        }
+        // Create User Get Method
+        public ViewResult CreateAccount()
+        {
+            return View();
+        }
+        // Create User Post Method
+        [HttpPost]
+        public async Task<IActionResult> CreateAccount(AccountViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+                IdentityResult result
+                    = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+        // Edit Account Get Method
+        public async Task<IActionResult> EditAccount(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        // Edit Account Post Method
+        [HttpPost]
+        public async Task<IActionResult> EditAccount(string id, string email,
+                string password)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                user.Email = email;
+                IdentityResult validEmail
+                    = await userValidator.ValidateAsync(userManager, user);
+                if (!validEmail.Succeeded)
+                {
+                    AddErrorsFromResult(validEmail);
+                }
+                IdentityResult validPass = null;
+                if (!string.IsNullOrEmpty(password))
+                {
+                    validPass = await passwordValidator.ValidateAsync(userManager,
+                        user, password);
+                    if (validPass.Succeeded)
+                    {
+                        user.PasswordHash = passwordHasher.HashPassword(user,
+                            password);
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(validPass);
+                    }
+                }
+                if ((validEmail.Succeeded && validPass == null)
+                        || (validEmail.Succeeded
+                        && password != string.Empty && validPass.Succeeded))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(result);
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View(user);
+        }
+        // Delete Account Post Method
+        [HttpPost]
+        public async Task<IActionResult> DeleteAccount(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View("Index", userManager.Users);
+        }
+
+        /*
+         * Role Administration
+         */
+
+        // ShowRoles currently not available due to ShowRoles View not
+        //  recognizing IEnumerable<IdentityRole>. Login locked for now
+        public ViewResult ShowRoles() => View(roleManager.Roles);
+
+        [Authorize(Roles = "Admins")]
+        public IActionResult CreateRole() => View();
+
+        // Create role Post method
+        [HttpPost]
+        public async Task<IActionResult> CreateRole([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result
+                    = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(name);
+        }
+        // Delete role Post method
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "No role found");
+            }
+            return View("Index", roleManager.Roles);
+        }
+        // Edit role
+        public async Task<IActionResult> EditRole(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            List<AppUser> members = new List<AppUser>();
+            List<AppUser> nonMembers = new List<AppUser>();
+            foreach (AppUser user in userManager.Users)
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name)
+                ? members : nonMembers;
+                list.Add(user);
+            }
+            return View(new RoleEditModel
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+        // Edit role Post method
+        [HttpPost]
+        public async Task<IActionResult> EditRole(RoleModificationModel model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (string userId in model.IdsToAdd ?? new string[] { })
+                {
+                    AppUser user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userManager.AddToRoleAsync(user,
+                        model.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            AddErrorsFromResult(result);
+                        }
+                    }
+                }
+
+                foreach (string userId in model.IdsToDelete ?? new string[] { })
+                {
+                    AppUser user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userManager.RemoveFromRoleAsync(user,
+                        model.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            AddErrorsFromResult(result);
+                        }
+                    }
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return await EditRole(model.RoleId);
+            }
+        }
+
+        // method to add errors via IdentityResult object
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+    }
+}
